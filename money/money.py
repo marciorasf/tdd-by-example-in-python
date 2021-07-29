3ae95031a29bf9b435b981408ecdfe515769ceb0
marciorasf <marciorasf@gmail.com>
2021-07-29 19:05:44 -0300

    add money addition

--- money/money.py
+++ money/money.py
@@ -2,7 +2,23 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
-class Money:
+class Expression(ABC):
+    @abstractmethod
+    def reduce(self, to: str) -> Money:
+        pass
+
+
+class Sum(Expression):
+    def __init__(self, augend: Money, addend: Money) -> None:
+        self.augend = augend
+        self.addend = addend
+
+    def reduce(self, to: str) -> Money:
+        amount = self.augend._amount + self.addend._amount
+        return Money(amount, to)
+
+
+class Money(Expression):
     def __init__(self, amount: int, currency: str) -> None:
         self.currency = currency
         self._amount: int = amount
@@ -23,3 +39,14 @@ class Money:
 
     def times(self, multiplier: int) -> Money:
         return Money(self._amount * multiplier, self.currency)
+
+    def plus(self, addend: Money) -> Expression:
+        return Sum(self, addend)
+
+    def reduce(self, to: str):
+        return self
+
+
+class Bank:
+    def reduce(self, source: Expression, to: str) -> Money:
+        return source.reduce(to)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,5 @@
-from money.money import Money
+from __future__ import annotations
+from money.money import Bank, Money, Sum
 
 
 def test_dollar_multiplication():
@@ -17,3 +18,32 @@ def test_equality():
 def test_currency():
     assert Money.dollar(1).currency == "USD"
     assert Money.franc(1).currency == "CHF"
+
+
+def test_simple_addition():
+    five = Money.dollar(5)
+    money_sum = five.plus(five)
+    bank = Bank()
+    reduced = bank.reduce(money_sum, "USD")
+    assert reduced == Money.dollar(10)
+
+
+def test_plus_returns_sum():
+    five = Money.dollar(5)
+    money_sum: Sum = five.plus(five)
+
+    assert money_sum.augend == five
+    assert money_sum.augend == five
+
+
+def test_reduce_sum():
+    money_sum = Sum(Money.dollar(3), Money.dollar(4))
+    bank = Bank()
+    result = bank.reduce(money_sum, "USD")
+    assert result == Money.dollar(7)
+
+
+def test_reduce_money():
+    bank = Bank()
+    result = bank.reduce(Money.dollar(1), "USD")
+    assert result == Money.dollar(1)
