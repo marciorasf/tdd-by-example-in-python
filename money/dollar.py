e28894c5bb5e4b82e5a4c7f4f5b4e0b64a29a7d1
marciorasf <marciorasf@gmail.com>
2021-07-29 11:55:15 -0300

    make Dollar.amount private

--- money/dollar.py
+++ money/dollar.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 
 class Dollar:
     def __init__(self, amount: int) -> None:
-        self.amount: int = amount
+        self._amount: int = amount
 
     def times(self, multiplier: int) -> Dollar:
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
 
     def __eq__(self, object: object) -> bool:
         if isinstance(object, Dollar):
-            return self.amount == object.amount
+            return self._amount == object._amount
 
         return False
