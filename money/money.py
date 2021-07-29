6161c25374fbd92d1fc9b7d722c4018d010029fc
marciorasf <marciorasf@gmail.com>
2021-07-29 13:08:32 -0300

    replace franc and dollar on testss with Money factory methods

--- money/money.py
+++ money/money.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 
 
-class Money:
+class Money(ABC):
     def __init__(self, amount: int) -> None:
         self._amount: int = amount
 
@@ -10,6 +11,18 @@ class Money:
             return self._amount == object._amount and self.__class__ == object.__class__
 
         return False
+
+    @staticmethod
+    def dollar(amount: int) -> Dollar:
+        return Dollar(amount)
+
+    @staticmethod
+    def franc(amount: int) -> Franc:
+        return Franc(amount)
+
+    @abstractmethod
+    def times(self, multiplier: int) -> Money:
+        pass
 
 
 class Dollar(Money):
